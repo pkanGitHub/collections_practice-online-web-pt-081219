bad3ee75a58933f2e37468d6fd1c9daae0ae5d4a
Phyllis Kan <pyk9q7@gmail.com>
2019-08-21 16:27:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x|x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each{|x| x[2] = "$"}
+
+end
+
+# def find_a(array)
+#   array.include?("a")
+# end
+
+# def sum_array(array)
+#
+# end
+#
+# def add_s(array)
+#
+# end

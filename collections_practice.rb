9e9420c3dbe3d96a26bebe2a078271d05b79c0b7
Phyllis Kan <pyk9q7@gmail.com>
2019-08-21 18:44:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect do {|element, index|}
    if element != "feet"
       element << "s"
     end

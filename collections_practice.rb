4d2ddc365a3687ef55e8abe30c7b55f820b540c5
Phyllis Kan <pyk9q7@gmail.com>
2019-08-21 18:28:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,5 +49,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element[] << "s"}
+  array.each_with_index.collect{|element, index| element[2] << "s"}
 end
